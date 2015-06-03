8463d3e0e285a9010c51c91b689669e14652e157
dingli <dingli@iMac.local>
2015-06-03 15:31:45 +0800

    add show history  ...

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -21,7 +21,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.textfield.identify = TEXT_ID;
-    
+
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"cell"];
 }
 
@@ -29,26 +29,29 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)hide:(id)sender {
+    [self.textfield hideHistroy];
+}
+
+- (IBAction)show:(id)sender {
+    [self.textfield showHistory];
+}
+
 - (IBAction)save:(id)sender {
     [self.textfield synchronize];
-
-    [self.tableView reloadData];
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section; {
-    return [[self.textfield loadHistroy] count];
+- (void)textFieldDidBeginEditing:(UITextField *)textField;{
+    if (textField == self.textfield) {
+        [textField showHistory];
+    }
 }
 
-// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath; {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
-    NSArray* data = [self.textfield loadHistroy];
-    cell.textLabel.text = [data objectAtIndex:indexPath.row];
-    
-    return cell;
+- (void)textFieldDidEndEditing:(UITextField *)textField; {
+    if (textField == self.textfield) {
+        [textField hideHistroy];
+    }
 }
-
 
 @end
